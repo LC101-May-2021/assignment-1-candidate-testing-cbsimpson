67d2f26698462e94637a98c5dcf5f35a912e23ee
Chris Simpson <>
2021-05-24 18:51:49 +0000

    Completed 1.1; Minor edits

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = " ";
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -15,7 +15,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = String (input.question("Enter your name: "));
+candidateName = input.question("Enter your name: ");
 }
 
 function askQuestion() {
