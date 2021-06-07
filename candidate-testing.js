b4ea25ea3834ebe009e5e8b3e81e423724919a2f
Chris Simpson <>
2021-06-07 02:49:14 +0000

    Modified code so that grade returns a percentage auto grader. Also added space after string in variable question.

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,7 +5,7 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American woman in space?";
+let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
@@ -70,16 +70,18 @@ Correct Answer: ${correctAnswers[4]}`);
 >>> Status: ${quizStatus} <<<
 `)
 
+  grade = grade/5*100;
+
   return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
- console.log("Hello " + candidateName); 
+  console.log("Hello " + candidateName); 
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
+  }
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
